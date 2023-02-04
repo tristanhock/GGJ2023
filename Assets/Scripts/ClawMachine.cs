52bd5ab668d87acbd8d8199d895439393402944c
steph0saurs <stephaniem131313@gmail.com>
2023-02-04 18:13:44 -0500

    Initial commit

---
+++ Assets/Scripts/ClawMachine.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class ClawMachine : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    //claw movement so like drop, pick up, left n right
+    //1 coin = 3 credits 
+    void Update()
+    {
+        
+    }
+}
