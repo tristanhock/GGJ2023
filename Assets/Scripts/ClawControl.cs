a6cd9a2f0b090f5094ed6f4d0a84326a2be70945
Cassandra Carpenter <carpenterclynn@gmail.com>
2023-02-04 19:48:21 -0500

    Update ClawControl.cs

--- Assets/Scripts/ClawControl.cs
+++ Assets/Scripts/ClawControl.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ClawControl : MonoBehaviour
 {
+    private bool _up, _down;
+
+    private bool _left, _right; 
     // Start is called before the first frame update
     void Start()
     {
@@ -15,20 +18,61 @@ public class ClawControl : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
+            _down = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            _down = false;
         }
 
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            
+            _up = true;
         }
 
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            _up = false; 
+        }
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
+            _left = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            _left = false;
         }
 
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            
+            _right = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            _right = false; 
+        }
+
+        if (_up)
+        {
+            gameObject.transform.Translate(0,-1f, 0);
+        }
+
+        if (_down)
+        {
+            gameObject.transform.Translate(0, 1f,0);
+        }
+
+        if (_right)
+        {
+            gameObject.transform.Translate(0.3f, 0,0);
+        }
+
+        if (_left)
+        {
+            gameObject.transform.Translate(-0.3f, 0,0);
         }
     }
 }
