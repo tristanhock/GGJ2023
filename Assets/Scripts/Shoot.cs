6b6816f2271a3e6fe8704cbc3c9a1612a4ab2741
Cassandra Carpenter <carpenterclynn@gmail.com>
2023-02-04 22:34:32 -0500

    code

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
+    public GameObject _toy;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +19,6 @@ public class Shoot : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(Collision.gameObject);
+       // Destroy(Collision.gameObject);
     }
 }
