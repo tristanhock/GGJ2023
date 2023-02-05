be7a8fd508d59cb8ea63761416d93132548ec059
Cassandra Carpenter <carpenterclynn@gmail.com>
2023-02-05 11:17:58 -0500

    bhdsjb

--- Assets/Scripts/ClawControl.cs
+++ Assets/Scripts/ClawControl.cs
@@ -8,14 +8,22 @@ public class ClawControl : MonoBehaviour
     private bool _close;
     public bool _open;
     private bool _left, _right;
+    
+    public float openSpeed = 1.0f; // Speed at which the claw opens
+    public float closeSpeed = 1.0f; // Speed at which the claw closes
+    
 
-    Rigidbody2D _rclaw, _lclaw;
+    public bool isClawOpen = false; // Keeps track of whether the claw is open or closed
+    private float currentClawOpen = 0.0f; // Keeps track of the current claw open distance
+    public Transform _lclawopen, _lclawclose, _rclawopen, _rclawclose;
+    public Transform _rclaw, _lclaw;
+
     // Start is called before the first frame update
     void Start()
     {
-        _rclaw = GameObject.Find("right claw").GetComponent<Rigidbody2D>();
-        _lclaw = GameObject.Find("left claw").GetComponent<Rigidbody2D>();
+        
         _open = false;
+
     }
 
     // Update is called once per frame
@@ -38,8 +46,9 @@ public class ClawControl : MonoBehaviour
 
         if (Input.GetKeyUp(KeyCode.UpArrow))
         {
-            _up = false; 
+            _up = false;
         }
+
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             _left = true;
@@ -57,57 +66,93 @@ public class ClawControl : MonoBehaviour
 
         if (Input.GetKeyUp(KeyCode.RightArrow))
         {
-            _right = false; 
+            _right = false;
         }
+
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            _open = !_open; 
+            _open = !_open;
+            isClawOpen = !isClawOpen;
         }
 
         if (_up)
         {
-            gameObject.transform.Translate(0,-0.01f, 0);
+            gameObject.transform.Translate(0, -0.01f, 0);
         }
 
         if (_down)
         {
-            gameObject.transform.Translate(0, 0.01f,0);
+            gameObject.transform.Translate(0, 0.01f, 0);
         }
 
         if (_right)
         {
-            gameObject.transform.Translate(0.01f, 0,0);
+            gameObject.transform.Translate(0.01f, 0, 0);
         }
 
         if (_left)
         {
-            gameObject.transform.Translate(-0.01f, 0,0);
+            gameObject.transform.Translate(-0.01f, 0, 0);
         }
 
-        if (_open)
+        // if (_open)
+        // {
+        //     Debug.Log("left claw" + _lclaw.transform.eulerAngles);
+        //     Debug.Log("right claw" + _rclaw.transform.eulerAngles);
+        //     if (_lclaw.transform.eulerAngles.z > 300)
+        //     {
+        //         _lclaw.transform.Rotate(0, 0, 1f);
+        //     }
+        //
+        //     if (_lclaw.transform.eulerAngles.z < 60)
+        //     {
+        //         _lclaw.transform.Rotate(0, 0, 1f);
+        //     }
+        // }
+        //
+        // if (!_open)
+        // {
+        //     if (_rclaw.transform.eulerAngles.z > 300)
+        //     {
+        //         _rclaw.transform.Rotate(0, 0, -1f);
+        //     }
+        //
+        //     if (_rclaw.transform.eulerAngles.z < 60)
+        //     {
+        //         _rclaw.transform.Rotate(0, 0, 1f);
+        //     }
+        // }
+
+
+
+
+        // Check if the claw should be open or closed
+        if (isClawOpen)
         {
-            
-            if (_lclaw.transform.eulerAngles.z > 300)
-            {
-                _lclaw.transform.Rotate(0,0,1f);
-            }
-            if(_lclaw.transform.eulerAngles.z < 60)
-            {
-                _lclaw.transform.Rotate(0,0,1f);
-            }
-
-            if (!_open)
-            {
-                if (_rclaw.transform.eulerAngles.z > 300)
-                {
-                    _rclaw.transform.Rotate(0, 0, -1f);
-                }
-
-                if (_rclaw.transform.eulerAngles.z < 60)
-                {
-                    _rclaw.transform.Rotate(0, 0, 1f);
-                }
-            }
+            _lclaw.transform.rotation = Quaternion.Lerp(_lclaw.rotation, _lclawopen.rotation, openSpeed * Time.deltaTime);
+            _rclaw.transform.rotation = Quaternion.Lerp(_rclaw.rotation, _rclawopen.rotation, openSpeed * Time.deltaTime);
         }
+        
+        else
+        {
+            _lclaw.transform.rotation = Quaternion.Lerp(_lclaw.rotation, _lclawclose.rotation, openSpeed * Time.deltaTime);
+            _rclaw.transform.rotation = Quaternion.Lerp(_rclaw.rotation, _rclawclose.rotation, openSpeed * Time.deltaTime);
+        }
+
+        // Apply the claw open distance to the claw object
+        //claw.localPosition = new Vector3(0.0f, currentClawOpen, 0.0f);
+
+    }
+
+    public void OpenClaw()
+    {
+        isClawOpen = true;
+    }
+
+    public void CloseClaw()
+    {
+        isClawOpen = false;
     }
 }
+      
+    
--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -1,91 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-
-public class DestroyPointScripts : MonoBehaviour
+public class Shoot : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    // Start is called before the first frame update
+    void Start()
     {
-        if (collision.tag == "ddols")
-        {
-            switch (collision.gameObject.name)
-            {
-                case "ddols1(Clone)":
-                    GameManager.Instance.Score = GameManager.Instance.Score + (GameManager.Instance.ScoreMul * 1);
-                    break;
-                case "ddols2(Clone)":
-                    GameManager.Instance.Score = GameManager.Instance.Score + (GameManager.Instance.ScoreMul * 2);
-                    break;
-                case "ddols3(Clone)":
-                    GameManager.Instance.Score = GameManager.Instance.Score + (GameManager.Instance.ScoreMul * 3);
-                    break;
-                case "ddols4(Clone)":
-                    GameManager.Instance.Score = GameManager.Instance.Score + (GameManager.Instance.ScoreMul * 4);
-                    break;
-                case "ddols5(Clone)":
-                    GameManager.Instance.Score = GameManager.Instance.Score + (GameManager.Instance.ScoreMul * 5);
-                    break;
-            }
-            Destroy(collision.gameObject);
-            EffectMaker.Instance.MakeOfEffect(1);
-            GameManager.Instance.dollsCount--;
-            SoundManager.Instance._DeadSound();
-            
-        }
+        
+    }
 
-        if (collision.tag == "item")
-        {
-            switch (collision.gameObject.name)
-            {//  angle = (angle > 180) ? angle - 360 : angle;
-                case "item1(Clone)":
-                    if (GameManager.Instance.isMulScore)
-                        GameManager.Instance.MulTime += 3;
-                    else
-                    {
-                        GameManager.Instance.ScoreMul = 2;
-                        StartCoroutine(GameManager.Instance.MulScore());
-                    }
-                    break;
-                case "item2(Clone)":
-                    if (GameManager.Instance.isMulScore)
-                    {
-                        if (GameManager.Instance.ScoreMul < 3)
-                        {
-                            GameManager.Instance.ScoreMul = 3;
-                            StartCoroutine(GameManager.Instance.MulScore());
-                        }
-                        else
-                            GameManager.Instance.MulTime += 3;
-                    }
-                    else
-                    {
-                        GameManager.Instance.ScoreMul = 3;
-                        StartCoroutine(GameManager.Instance.MulScore());
-                    }
-                    break;
-                case "item3(Clone)":
-                    if (UIManager.Instance.GameTimer.value <= UIManager.Instance.GameTimer.maxValue)
-                    {
-                        if (GameManager.Instance.GameTime <= UIManager.Instance.GameTimer.maxValue)
-                        {
-                            GameManager.Instance.GameTime += 3f;
-                            UIManager.Instance.GameTimer.value += 3f;
-                            Debug.Log(UIManager.Instance.GameTimer.value);
-                        }
-                    }
-                    break;
-                case "item4(Clone)":
-                    GameManager.Instance.PowerTime = +5f;
-                    break;
-            }
-            Destroy(collision.gameObject);
-            EffectMaker.Instance.MakeOfEffect(2);
-            GameManager.Instance.itemdollsCount--;
-            SoundManager.Instance._DeadSound();
-            //Destroy(Effect);
-
-        }
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
